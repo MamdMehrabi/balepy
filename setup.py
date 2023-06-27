61f5198a09b4c5fe0a91dd01f37eca2c369ad431
Mamad Mehrabi Rad <121286220+OnlyRad@users.noreply.github.com>
2023-06-27 23:38:26 +0330

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'balepy',
+    version = '0.6',
+    author='Mohammad and Erfan',
+    author_email = 'mohammadmehrabi175@gmail.com',
+    description = 'balepy a Library Python for create bot API in bale application',
+    keywords = ['bot' , 'Bot' , 'bale' , 'robot'],
+    long_description = open("README.md", encoding="utf-8").read(),
+    python_requires="~=3.6",
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/OnlyRad/bale',
+    packages = find_packages(),
+    install_requires = [],
+    classifiers = [
+    	"Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
