b78c41490771a2e577ddc7e047fb015a1738d461
amirali irvany <dev.amirali.irvany@gmail.com>
2023-12-12 19:52:49 +0330

    Update __init__.py

--- balepy/__init__.py
+++ balepy/__init__.py
@@ -1,6 +1,1 @@
-from balepy.main import Client
-from balepy.Messages import messages
-
-app = Client('905219034:yV3KyeqlSQDHPLF38vX6t2ychdCLSSAXEsRnngy7')
-s = app.polling()
-print(s)
+from client import Client
