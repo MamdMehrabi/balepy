de6c6d8fe2908c6033fbc65fd17aa7ba9da1248b
amirali irvany <dev.amirali.irvany@gmail.com>
2024-01-19 12:39:53 +0330

    Update __init__.py

--- balepy/__init__.py
+++ balepy/__init__.py
@@ -1,1 +1,9 @@
 from client import Client
+
+__title__: str = 'balepy'
+__version__: str = '1.1.1'
+__license__: str = 'MIT license'
+__author__: str = 'Mamad Mehrabi Rad'
+__url__: str = 'https://github.com/OnlyRad'
+__author_email__: str = 'mohammadmehrabi175@gmail.com'
+__description__: str = 'Optimal and practical module for building API bots in bale messengers'
