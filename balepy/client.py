a24914399dab0c440b0e3514985efb92d3f7d44f
amirali irvany <dev.amirali.irvany@gmail.com>
2024-02-27 20:33:18 +0330

    Update client.py

--- balepy/client.py
+++ balepy/client.py
@@ -1,25 +1,32 @@
-from util import message
+from .util import message
 import requests
-
+import aiohttp
 
 class Client:
 
     def __init__(self, token: str, timeout: float = 20) -> None:
         self.token: str = token
         self.timeout: float = timeout
+        self.form_data = aiohttp.FormData()
 
         if not token:
             raise ValueError('`token` did\'t passed')
 
-    def execute(self, method: str, data: dict = None) -> dict:
+    async def execute(self, method: str, data: dict = None) -> dict:
         url: str = f'https://tapi.bale.ai/bot{self.token}/{method}'
+        async with aiohttp.ClientSession() as session:
+            async with session.request('post', url=url, data=data, timeout=self.timeout) as r:
+                return await r.json()
+
+    def _get_updates(self, data: dict) -> dict:
+        url: str = f'https://tapi.bale.ai/bot{self.token}/getupdates'
         with requests.session() as session:
             with session.request('post', url=url, data=data, timeout=self.timeout) as r:
                 return r.json()
 
-    def request(self, method: str, data: dict = None) -> dict:
+    async def request(self, method: str, data: dict = None) -> dict:
         try:
-            return self.execute(method=method, data=data)
+            return await self.execute(method=method, data=data)
         except Exception as err:
             print(__file__, err, __file__)
 
@@ -27,27 +34,27 @@ class Client:
         '''Use this method to receive updates
         Example:
             from balepy import Client
+            import asyncio
 
             client = Client('token', timeout=10)
             def main():
                 for update in client.on_message():
                     print(update.text)
 
-            main()
+            asyncio.run(main())
         '''
         payload: dict = {
             'offset': -1, 'limit': 100
         }
         while True:
-            update = self.request('getupdates', data=payload)
+            update = self._get_updates(data=payload)
             payload['offset'] = 1
             if (update != None) and (update['ok']) and (update['result'] != []):
                 break
 
-        payload['offset'] = update['result'][len(update['result'])-1]['update_id'] + 1
-        payload['limit'] = 1
+        payload['offset'], payload['limit'] = update['result'][len(update['result'])-1]['update_id'], 1
         while True:
-            responce = self.request('getupdates', data=payload)
+            responce = self._get_updates(data=payload)
             if responce != None and responce['result'] != []:
                 payload['offset'] += 1
                 yield message(responce['result'][0])
@@ -78,7 +85,8 @@ class Client:
             'reply_markup': reply_markup,
             'reply_to_message_id': reply_to_message_id
         }
-        return self.request('sendmessage', data=payload)
+        return await self.request('sendmessage', data=payload)
+
 
     async def edit_message(
             self,
@@ -93,7 +101,7 @@ class Client:
             'reply_to_message_id': message_id,
             'reply_markup': reply_markup
         }
-        return self.request('editmessage', data=payload)
+        return await self.request('editmessage', data=payload)
 
 
     async def forward_message(
@@ -105,14 +113,14 @@ class Client:
         payload: dict = {
             'chat_id': chat_id, 'from_chat_id': from_chat_id, 'message_id': message_id
         }
-        return self.request('forwardmessage', json=payload)
+        return await self.request('forwardmessage', data=payload)
 
 
     async def delete_message(self, chat_id: str | int, message_id: int) -> dict:
         payload: dict = {
             'chat_id': chat_id, 'reply_to_message_id': message_id
         }
-        return self.request('deletemessage', data=payload)
+        return await self.request('deletemessage', data=payload)
 
 
     async def send_contact(
@@ -144,7 +152,7 @@ class Client:
             'last_name': last_name,
             'reply_to_message_id': reply_to_message_id
         }
-        return self.request('sendcontact', data=payload)
+        return await self.request('sendcontact', data=payload)
 
 
     async def send_photo(
@@ -156,21 +164,13 @@ class Client:
             reply_to_message_id: int = None,
             reply_markup: int = None
     ) -> dict:
-        files: dict = {
-            'photo': open(file, 'rb')
-        }
-        values: dict = {
-            'chat_id': chat_id,
-            'from_chat_id': from_chat_id,
-            'caption': caption,
-            'reply_to_message_id': reply_to_message_id,
-            'reply_markup': reply_markup
-        }
-        return requests.request(
-            'post',
-            url=f'https://tapi.bale.ai/bot{self.token}/sendphoto',
-            data=values, files=files, timeout=self.timeout
-        )
+        self.form_data.add_field('photo', open(file, 'rb'))
+        self.form_data.add_field('chat_id', str(chat_id))
+        self.form_data.add_field('caption', caption) if caption != None else caption
+        self.form_data.add_field(
+            'reply_to_message_id', str(reply_to_message_id)
+        ) if reply_to_message_id != None else reply_to_message_id
+        return await self.request('sendphoto', data=self.form_data)
 
 
     async def send_audio(
@@ -181,20 +181,13 @@ class Client:
             reply_to_message_id: int = None,
             reply_markup: str = None
     ) -> dict:
-        files: dict = {
-            'audio': open(file, 'rb')
-        }
-        values: dict = {
-            'chat_id': chat_id,
-            'caption': caption,
-            'reply_to_message_id': reply_to_message_id,
-            'reply_markup': reply_markup
-        }
-        return requests.request(
-            'post',
-            url=f'https://tapi.bale.ai/bot{self.token}/audio',
-            data=values, files=files, timeout=self.timeout
-        )
+        self.form_data.add_field('audio', open(file, 'rb'))
+        self.form_data.add_field('chat_id', str(chat_id))
+        self.form_data.add_field('caption', caption) if caption != None else caption
+        self.form_data.add_field(
+            'reply_to_message_id', str(reply_to_message_id)
+        ) if reply_to_message_id != None else reply_to_message_id
+        return await self.request('sendaudio', data=self.form_data)
 
 
     async def send_document(
@@ -219,20 +212,13 @@ class Client:
         :return:
             If successful, the sent message will be returned
         '''
-        files: dict = {
-            'document': open(file, 'rb')
-        }
-        values: dict = {
-            'chat_id': chat_id,
-            'caption': caption,
-            'reply_to_message_id': reply_to_message_id,
-            'reply_markup': reply_markup
-        }
-        return requests.request(
-            'post',
-            url=f'https://tapi.bale.ai/bot{self.token}/senddocument',
-            data=values, files=files, timeout=self.timeout
-        )
+        self.form_data.add_field('document', open(file, 'rb'))
+        self.form_data.add_field('chat_id', str(chat_id))
+        self.form_data.add_field('caption', caption) if caption != None else caption
+        self.form_data.add_field(
+            'reply_to_message_id', str(reply_to_message_id)
+        ) if reply_to_message_id != None else reply_to_message_id
+        return await self.request('senddocument', data=self.form_data)
 
 
     async def send_animation(
@@ -243,19 +229,14 @@ class Client:
             reply_markup: int = None
     ) -> dict:
         '''This method is used to send animation files (GIF video or H.264/MPEG-4 AVC without sound)'''
-        files: dict = {
-            'animation': open(file, 'rb')
-        }
-        values: dict = {
-            'chat_id': chat_id,
-            'reply_to_message_id': reply_to_message_id,
-            'reply_markup': reply_markup
-        }
-        return requests.request(
-            'post',
-            url=f'https://tapi.bale.ai/bot{self.token}/sendanimation',
-            data=values, files=files, timeout=self.timeout
-        )
+        self.form_data.add_field('animation', open(file, 'rb'))
+        self.form_data.add_field('chat_id', str(chat_id))
+        self.form_data.add_field('caption', caption) if caption != None else caption
+        self.form_data.add_field(
+            'reply_to_message_id', str(reply_to_message_id)
+        ) if reply_to_message_id != None else reply_to_message_id
+        return await self.request('sendanimation', data=self.form_data)
+
 
 
     async def send_voice(
@@ -266,20 +247,13 @@ class Client:
             reply_to_message_id: int = None,
             reply_markup: int = None
     ) -> dict:
-        files: dict = {
-            'voice': open(file, 'rb')
-        }
-        values: dict = {
-            'chat_id': chat_id,
-            'caption': caption,
-            'reply_to_message_id': reply_to_message_id,
-            'reply_markup': reply_markup
-        }
-        return requests.request(
-            'post',
-            url=f'https://tapi.bale.ai/bot{self.token}/sendvoice',
-            data=values, files=files, timeout=self.timeout
-        )
+        self.form_data.add_field('voice', open(file, 'rb'))
+        self.form_data.add_field('chat_id', str(chat_id))
+        self.form_data.add_field('caption', caption) if caption != None else caption
+        self.form_data.add_field(
+            'reply_to_message_id', str(reply_to_message_id)
+        ) if reply_to_message_id != None else reply_to_message_id
+        return await self.request('sendvoice', data=self.form_data)
 
 
     async def send_location(
@@ -316,11 +290,7 @@ class Client:
             'reply_to_message_id': reply_to_message_id,
             'reply_markup': reply_markup
         }
-        return requests.request(
-            'post',
-            url=f'https://tapi.bale.ai/bot{self.token}/sendlocation',
-            data=values, files=files, timeout=self.timeout
-        )
+        return await self.request('sendlocation', data=payload)
 
 
     async def set_webhook(self, url: str) -> dict:
@@ -328,16 +298,16 @@ class Client:
         payload: dict = {
             'url': url
         }
-        return self.request('setwebhook', data=payload)
+        return await self.request('setwebhook', data=payload)
 
 
     async def delete_webhook(self) -> dict:
-        return self.request(method='deletewebhook')
+        return await self.request(method='deletewebhook')
 
 
     async def get_webhook_info(self) -> dict:
         '''Use this method to get the current state of the webhook'''
-        return self.request(data='getwebhookinfo')
+        return await self.request(data='getwebhookinfo')
 
 
     async def webhook_info(self, url: str) -> dict:
@@ -345,12 +315,12 @@ class Client:
         payload: dict = {
             'url': url
         }
-        return self.request('webhookinfo', data=payload)
+        return await self.request('webhookinfo', data=payload)
 
 
     async def get_me(self) -> dict:
         '''get bot account information'''
-        return self.request(method='getme')
+        return await self.request(method='getme')
 
 
     async def get_chat(self, chat_id: str) -> dict:
@@ -365,7 +335,7 @@ class Client:
         payload: dict = {
             'chat_id': chat_id
         }
-        return self.request('getchat', data=payload)
+        return await self.request('getchat', data=payload)
 
 
     async def leave_chat(self, chat_id: str) -> dict:
@@ -378,14 +348,14 @@ class Client:
         payload: dict = {
             'chat_id': chat_id
         }
-        return self.request('leavechat', data=payload)
+        return await self.request('leavechat', data=payload)
 
 
     async def get_updates(self, offset: int = 0, limit: int = 0) -> dict:
         payload: dict = {
             'offset': offset, 'limit': limit
         }
-        return self.request('getupdates', data=payload)
+        return await self.request('getupdates', data=payload)
 
 
     async def get_chat_administrators(self, chat_id: str, just_get_id: bool = False) -> dict:
@@ -399,24 +369,23 @@ class Client:
             for user in responce['result']:
                 ids.append(user['user']['id'])
 
-            return ids
+            return await ids
         else:
-            return responce
-
+            return await responce
 
 
     async def get_chat_members_count(self, chat_id: str) -> dict:
         payload: dict = {
             'chat_id': chat_id
         }
-        return self.request('getchatmemberscount', data=payload)
+        return await self.request('getchatmemberscount', data=payload)
 
 
     async def get_chat_member(self, chat_id: str, user_id: str) -> dict:
         payload: dict = {
             'chat_id': chat_id, 'user_id': user_id
         }
-        return self.request('getchatmember', data=payload)
+        return await self.request('getchatmember', data=payload)
 
 
     async def set_chat_photo(self, chat_id: str | int, photo = str | bytes) -> dict:
@@ -426,21 +395,21 @@ class Client:
         values: dict = {
             'chat_id': chat_id
         }
-        return self.request('setchatphoto', data=values, files=files)
+        return await self.request('setchatphoto', data=values, files=files)
 
 
     async def ban_chat_member(self, chat_id: str | int, user_id: str | int) ->  dict:
         payload: dict = {
             'chat_id': chat_id, 'user_id': user_id
         }
-        return self.request('banchatmember', data=payload)
+        return await self.request('banchatmember', data=payload)
 
 
     async def un_ban_chat_member(self, chat_id: str | int, user_id: str | int) -> dict:
         payload: dict = {
             'chat_id': chat_id, 'user_id': user_id
         }
-        return self.request('unbanchatmember', data=payload)
+        return await self.request('unbanchatmember', data=payload)
 
 
     async def get_file(self, file_id: str) -> dict:
@@ -454,4 +423,4 @@ class Client:
         payload: dict = {
             'file_id': file_id
         }
-        return self.request('getfile', data=payload)
+        return await self.request('getfile', data=payload)
