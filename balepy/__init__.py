51ff25930aa642f6735413df49b6918fccc27068
Mamad Mehrabi Rad <mohammadmehrabi175@gmail.com>
2024-02-29 19:49:18 -0500

    edit .gitignore file

--- balepy/__init__.py
+++ balepy/__init__.py
@@ -1,3 +1,11 @@
 from .client import Client
 from .util import message
-from .don import welcome
+
+__title__: str = 'balepy'
+__version__: str = '1.3.6'
+__license__: str = 'MIT license'
+__author__: str = 'Mamad Mehrabi Rad'
+__url__: str = 'https://github.com/OnlyRad/balepy'
+__author_email__: str = 'mohammadmehrabi175@gmail.com'
+__description__: str = 'Optimal and practical module for building API bots in bale messengers'
+welcome(f"{__title__} -> {__version__}\nGithub: {__url__}")
--- balepy/don.py
+++ balepy/don.py
@@ -1,11 +1,3 @@
-__title__: str = 'balepy'
-__version__: str = '1.3.6'
-__license__: str = 'MIT license'
-__author__: str = 'Mamad Mehrabi Rad'
-__url__: str = 'https://github.com/OnlyRad/balepy'
-__author_email__: str = 'mohammadmehrabi175@gmail.com'
-__description__: str = 'Optimal and practical module for building API bots in bale messengers'
-
 def welcome(text, time: float = 0.035):
     from time import sleep
     try:
@@ -16,4 +8,3 @@ def welcome(text, time: float = 0.035):
             print(char, end='', flush=True)
             sleep(float(time))
     print()
-welcome(f"{__title__} -> {__version__}\nGithub: {__url__}")
