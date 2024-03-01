25bd0308972d2acf3b8e59c6acfb83792c75fc27
Mamad Mehrabi Rad <mohammadmehrabi175@gmail.com>
2024-03-01 09:57:55 -0500

    add filters to on_message

--- balepy/__init__.py
+++ balepy/__init__.py
@@ -10,11 +10,3 @@ __author__: str = 'Mamad Mehrabi Rad'
 __url__: str = 'https://github.com/OnlyRad/balepy'
 __author_email__: str = 'mohammadmehrabi175@gmail.com'
 __description__: str = 'Optimal and practical module for building API bots in bale messengers'
-
-app = Client("786709312:KejLTbe8HXUBDg34n9jY6iSLJrof5bCU7HJeelkB")
-async def main():
-    for m in app.on_message(Filters.group):
-        print(m.message)
-
-if __name__ == '__main__':
-    run(main())
