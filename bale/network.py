18f68082e26c47bb89e3dadd272db74ea5533af5
amirali irvany <dev.amirali.irvany@gmail.com>
2024-03-31 20:55:00 +0330

    Update network.py

--- bale/network.py
+++ bale/network.py
@@ -4,12 +4,15 @@ class Network:
     def __init__(self, token: str, timeout: float):
         self.token: str = token
         self.timeout: float = timeout
-        self.session = requests.session()
+        self.session: function = requests.session()
 
     @property
     def url(self) -> str:
         return f'https://tapi.bale.ai/bot{self.token}/'
 
-    def connect(self, method: str, data: dict = None, request_method: str = 'post') -> requests.Response:
-        with self.session.request(method=request_method, url= self.url + method, timeout=self.timeout, data=data) as response:
+    def disconnect(self):
+        return self.session.close()
+
+    async def connect(self, method: str, data: dict = None, files: dict = None) -> requests.Response:
+        with self.session.request('post', url= self.url + method, timeout=self.timeout, data=data, files=files) as response:
             return response.json()
