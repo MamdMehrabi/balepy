c3a2e301310bc1d80a4097497a0038b5c992c49c
amirali irvany <dev.amirali.irvany@gmail.com>
2024-04-03 22:55:41 +0330

    Update examples/group_management.py

--- examples/group_management.py
+++ examples/group_management.py
@@ -13,14 +13,10 @@ async def update_admin_chat(client: Client, chat_id: int) -> list:
 
 
 async def check_allowed_message(client: Client, chat_id: int, text: str, user_id: int, message_id: int):
+    forbiden_texts = ['@', 'ble.ir', 'join']
     if user_id not in chat_admins:
-        if text in ['@', 'ble.ir', 'join']:
+        if text in forbiden_texts:
             await client.delete_message(chat_id=chat_id, message_id=message_id)
-
-
-async def get_chat_invite_link(client: Client, chat_id: int):
-    invite_link = await client.get_chat(chat_id=chat_id)
-    return invite_link['invite_link']
 
 
 async def startup_bot(client: Client, chat_id: int):
@@ -38,22 +34,22 @@ client = Client(__token, 20)
 async def main():
     # starting bot and send welcome message
     await startup_bot(client=client, chat_id=chat_id)
-    async for update in client.on_message():
+    async for message in client.on_message():
         await check_allowed_message(
-            client=client, chat_id=update.chat_id, text=update.text,
-            user_id=update.author_id, message_id=update.message_id
+            client=client, chat_id=message.chat_id, text=message.text,
+            user_id=message.author_id, message_id=message.message_id
         )
-        if update.text.lower() == '/test':
-            await update.reply('The bot is now active in your chat')
+        if message.text.lower() == '/test':
+            await message.reply('The bot is now active in your chat')
 
-        elif update.text.lower() == '/update-admins':
-            await update.reply('*Updating the list of admins...*')
-            await update_admin_chat(client=client, chat_id=update.chat_id)
-            await update.reply('*The list of admins has been successfully updated*')
+        elif message.text.lower() == '/update-admins':
+            await message.reply('*Updating the list of admins...*')
+            await update_admin_chat(client=client, chat_id=message.chat_id)
+            await message.reply('*The list of admins has been successfully updated*')
 
-        elif update.text.lower() == '/link':
-            invite_link = await get_chat_invite_link(client=client, chat_id=update.chat_id)
-            await update.reply('Your chat link:\n\n```%s```' % invite_link)
+        elif message.text.lower() == '/link':
+            invite_link = await client.get_chat_invite_link(chat_id=message.chat_id)
+            await message.reply('Your chat link:\n\n```%s```' % invite_link)
 
 
 if __name__ == '__main__':
