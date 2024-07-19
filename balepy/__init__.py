16ac6a57e8b5ffdcd8b92fd608f06f559fbdae99
AmirAli Irvani <irvaniamirali@proton.me>
2024-07-19 23:48:17 +0330

    Create __init__.py

--- balepy/__init__.py
+++ balepy/__init__.py
@@ -1,9 +1,1 @@
 from .client import Client
-
-__title__: str = 'balepy'
-__version__: str = '1.3.6'
-__license__: str = 'MIT license'
-__author__: str = 'Mamad Mehrabi Rad'
-__url__: str = 'https://github.com/balepy'
-__author_email__: str = 'mohammadmehrabi175@gmail.com'
-__description__: str = 'Optimal and practical module for building API bots in bale messengers'
