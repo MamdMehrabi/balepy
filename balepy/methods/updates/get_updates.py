94a500aa692880ff73ae9029fed7a16822e3e630
AmirAli Irvani <irvaniamirali@proton.me>
2024-07-21 23:49:15 +0330

    Update get_updates.py

--- balepy/methods/updates/get_updates.py
+++ balepy/methods/updates/get_updates.py
@@ -16,4 +16,5 @@ class GetUpdates:
             "offset": offset,
             "limit": limit
         }
-        return await self.api.execute(name="getUpdates", method=HTTPMethod.POST, data=params)
+        response_data = await self.api.execute(name="getUpdates", method=HTTPMethod.POST, data=params)
+        return response_data["result"]
