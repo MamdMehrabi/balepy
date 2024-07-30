2011d61cfd9980ed49e3e69c63f51486ba907ca1
MamdMehrabi <mohammadmehrabi175@gmail.com>
2024-07-30 17:41:45 +0330

    add send_document.py

--- balepy/methods/messages/__init__.py
+++ balepy/methods/messages/__init__.py
@@ -4,7 +4,9 @@ from .copy_message import CopyMessage
 from .delete_message import DeleteMessage
 from .edit_message_text import EditMessageText
 from .send_photo import SendPhoto
+from .send_audio import SendAudio
+from .send_document import SendDocument
 
 
-class Messages(SendMessage, ForwardMessage, CopyMessage, DeleteMessage, EditMessageText, SendPhoto):
+class Messages(SendMessage, ForwardMessage, CopyMessage, DeleteMessage, EditMessageText, SendPhoto, SendAudio, SendDocument):
     pass
