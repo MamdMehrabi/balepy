a510c0a8e44ac1eb0c61d2dc8a1cb433443600fb
MamdMehrabi <mohammadmehrabi175@gmail.com>
2024-07-30 17:27:38 +0330

    add send_photo.py

--- balepy/methods/messages/__init__.py
+++ balepy/methods/messages/__init__.py
@@ -3,7 +3,8 @@ from .forward_message import ForwardMessage
 from .copy_message import CopyMessage
 from .delete_message import DeleteMessage
 from .edit_message_text import EditMessageText
+from .send_photo import SendPhoto
 
 
-class Messages(SendMessage, ForwardMessage, CopyMessage, DeleteMessage, EditMessageText):
+class Messages(SendMessage, ForwardMessage, CopyMessage, DeleteMessage, EditMessageText, SendPhoto):
     pass
---
+++ balepy/methods/messages/send_photo.py
@@ -0,0 +1,24 @@
+from balepy.objects import HTTPMethod
+
+import balepy
+
+class SendPhoto:
+
+    async def send_photo(
+            self: "balepy.Client",
+            chat_id: str,
+            from_chat_id: str,
+            photo: str,
+            caption = None,
+            reply_to_message_id = None,
+            reply_markup = None
+    ):
+        params = {
+            'chat_id': chat_id,
+            'from_chat_id': from_chat_id,
+            'photo': photo,
+            'caption': caption,
+            'reply_to_message_id': reply_to_message_id,
+            'reply_markup': reply_markup
+        }
+        return await self.api.execute(name="sendPhoto", method=HTTPMethod.POST, data=params)
